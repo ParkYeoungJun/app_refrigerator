aa9b1744e66eee1eb9c59e76f49284d2ed237e69
nyh <nyh7105@naver.com>
2016-09-26 20:40:46 +0900

    OCR 기능 추가

--- app/src/main/java/com/example/younghyeon/test0901/MainActivity.java
+++ app/src/main/java/com/example/younghyeon/test0901/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.younghyeon.test0901;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -18,6 +26,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -30,7 +40,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -40,6 +55,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_FOOD_INPUT = 1001;
     public static final int REQUEST_CODE_FOOD_UPDATE = 1003;
+    public static final int REQUEST_CODE_CAMERA = 1004;
 
     final int STATE_FREEZER = 0;
     final int STATE_REFRIGERATOR = 1;
@@ -78,6 +96,25 @@ public class MainActivity extends AppCompatActivity {
     ArrayList foodList2;
     ArrayList foodList3;
 
+    public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/data/tesseract/";
+
+    // You should have the trained data file in assets folder
+    // You can get them at:
+    // http://code.google.com/p/tesseract-ocr/downloads/list
+    public static final String lang = "kor";
+
+    private static final String TAG = "SimpleAndroidOCR.java";
+
+//    protected Button _button;
+    protected String _path;
+    protected boolean _taken;
+
+    protected static final String PHOTO_TAKEN = "photo_taken";
+
+    ArrayList compareFoodList;
+    ArrayList resArrayList;
 
 
     public static GridViewAdapter mAdapter1;
@@ -98,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton orderButton;
     ImageButton cancelButton;
     ImageButton graphButton;
+    ImageButton cameraButton;
 
     String myJSON;
 
@@ -132,6 +170,8 @@ public class MainActivity extends AppCompatActivity {
         mAdapter1.foodArrayList = foodList1;
         mAdapter2.foodArrayList = foodList2;
         mAdapter3.foodArrayList = foodList3;
+
+        initCompareFoodList();
 //        mAdapter1 = new GridViewAdapter(MainActivity.this, );
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -175,7 +215,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v(TAG, "Starting Camera app");
+                startCameraActivity();
+            }
+        });
+        _path = DATA_PATH + "/ocr.jpg";
 
         moveButton = (ImageButton) findViewById(R.id.moveButton);
         moveButton.setVisibility(View.INVISIBLE);
@@ -546,6 +594,234 @@ public class MainActivity extends AppCompatActivity {
         getFood("http://52.78.88.182/getFood.php");
     }
 
+    public void initCompareFoodList(){
+        compareFoodList = new ArrayList<String>();
+        resArrayList = new ArrayList<String>();
+        compareFoodList.add("바나나");
+        compareFoodList.add("깻잎");
+        compareFoodList.add("대파");
+        compareFoodList.add("딸기");
+        compareFoodList.add("요플레");
+        compareFoodList.add("복숭아");
+        compareFoodList.add("우유");
+        compareFoodList.add("감자");
+        compareFoodList.add("김자반");
+        compareFoodList.add("미역");
+        compareFoodList.add("비피더스");
+        compareFoodList.add("뒷다리");
+        compareFoodList.add("새우젓");
+        compareFoodList.add("양배추");
+        compareFoodList.add("앞다리");
+        compareFoodList.add("쌀");
+        compareFoodList.add("무");
+        compareFoodList.add("연어");
+        compareFoodList.add("양지");
+        compareFoodList.add("마늘");
+        compareFoodList.add("오징어");
+        compareFoodList.add("버섯");
+        compareFoodList.add("송편");
+        compareFoodList.add("소");
+        compareFoodList.add("사과");
+        compareFoodList.add("순대");
+        compareFoodList.add("송편");
+        compareFoodList.add("블루");
+        compareFoodList.add("부침");
+        compareFoodList.add("시금치");
+        compareFoodList.add("방울토마토");
+        compareFoodList.add("부침");
+        compareFoodList.add("안성 탕면");
+        compareFoodList.add("소면");
+        compareFoodList.add("열무");
+        compareFoodList.add("얼갈이");
+        compareFoodList.add("쪽파");
+        compareFoodList.add("고추");
+        compareFoodList.add("밀키스");
+        compareFoodList.add("막걸리");
+        compareFoodList.add("식빵");
+        compareFoodList.add("냉면");
+        compareFoodList.add("애호박");
+        compareFoodList.add("닭");
+        compareFoodList.add("참외");
+
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+    }
+
+    protected void startCameraActivity() {
+        File file = new File(_path);
+        Uri outputFileUri = Uri.fromFile(file);
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(MainActivity.PHOTO_TAKEN, _taken);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(TAG, "onRestoreInstanceState()");
+        if (savedInstanceState.getBoolean(MainActivity.PHOTO_TAKEN)) {
+            onPhotoTaken();
+        }
+    }
+
+    protected void onPhotoTaken() {
+        _taken = true;
+        String tempStr = "";
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(_path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        // _image.setImageBitmap( bitmap );
+
+        Log.v(TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(DATA_PATH, lang);
+        baseApi.setImage(bitmap);
+
+        String recognizedText = baseApi.getUTF8Text();
+        //  baseApi.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-[]}{;:'\"\\|~`,./<>?");
+        baseApi.end();
+
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }
+
+        recognizedText = recognizedText.trim();
+
+//        if ( recognizedText.length() != 0 ) {
+//            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+//            tempStr += recognizedText;
+//            _field.setSelection(_field.getText().toString().length());
+//        }
+
+        if ( recognizedText.length() != 0 ) {
+//            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            tempStr += recognizedText;
+//            _field.setSelection(_field.getText().toString().length());
+        }
+        Toast.makeText(MainActivity.this, " "+ tempStr,Toast.LENGTH_LONG).show();
+        for (int i = 0; i < compareFoodList.size(); i++) {
+            if (tempStr.contains(compareFoodList.get(i).toString())){
+                resArrayList.add(compareFoodList.get(i).toString());
+            }
+        }
+        for (int i = 0; i < resArrayList.size(); i++) {
+            Log.e("check","res : "+resArrayList.get(i));
+        }
+
+        tempStr = "";
+
+//        Log.e("check","tempArrayList : "+tempArrayList.size());
+//        for (int i = 0; i < tempArrayList.size(); i++){
+//            Log.e("check","tempArrayList : "+tempArrayList.get(i));
+//        }
+
+        // Cycle done.
+    }
+
     public void setVisibleToCheckOff(){
         listButton.setVisibility(View.VISIBLE);
         plusButton.setVisibility(View.VISIBLE);
@@ -567,6 +843,7 @@ public class MainActivity extends AppCompatActivity {
         cancelButton.setVisibility(View.VISIBLE);
         moveButton.setVisibility(View.VISIBLE);
         removeButton.setVisibility(View.VISIBLE);
+        orderButton.setClickable(false);
         checkRemoveButtonVisibility = true;
     }
 
@@ -838,6 +1115,8 @@ public class MainActivity extends AppCompatActivity {
                 mAdapter3.notifyDataSetChanged();
             }
             Toast.makeText(getApplicationContext(), "수정되었습니다.", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == REQUEST_CODE_CAMERA) {
+            onPhotoTaken();
         }
     }
 
